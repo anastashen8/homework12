592fb68822e343709758f9318e6430be2c6a0765
Anastasiya <anastashen8@gmail.com>
2022-11-30 12:46:58 +0300

    add Final

---
+++ Final/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите строки через ',':");
+string[] array = Console.ReadLine().Split(' ',',');
+string[] newarray = new string[array.Length];
+
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+            Console.Write($"{array[i]} ");
+    }
+}
+
+FillArray (array, newarray);
+PrintArray(newarray);
